d99f6acbc22f69e3dd46dfb46857ded67679bed2
lunqwe <roma.boyko10068@gmail.com>
2023-12-15 21:53:40 +0200

    genres and types filters

--- main/homepage/views.py
+++ main/homepage/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from animepage.models import Anime, AdditionalName, Film
+from animepage.models import Anime, AdditionalName, Film, Genre
 from itertools import chain
 from django.core.paginator import Paginator
 from django.http import JsonResponse
@@ -7,8 +7,11 @@ from django.core.serializers.json import DjangoJSONEncoder
 import json
 from django.http import JsonResponse
 from django.views import View
+from django.db.models import Q
+from django.db.models import Count
 
-def insertion_sort(objects):
+
+def insertion_sort(objects, selected_genres=None):
     for i in range(1, len(objects)):
         key = objects[i]
         j = i - 1
@@ -16,6 +19,10 @@ def insertion_sort(objects):
             objects[j + 1] = objects[j]
             j -= 1
         objects[j + 1] = key
+        
+    if selected_genres:
+        objects = [obj for obj in objects if any(genre.id in selected_genres for genre in obj.genres.all())]
+
     return objects
    #2443
 # Create your views here.
@@ -25,30 +32,41 @@ def main(request):
     combined_objects = list(chain(anime, films))
     sorted_objects = insertion_sort(combined_objects)
     novelty_list = sorted_objects[:4]
-    return render(request, "homepage/main.html", {'anime_list' : sorted_objects, 'novelty_list' : novelty_list})
+    genres = Genre.objects.all()
+        
+    return render(request, "homepage/main.html", {'anime_list' : sorted_objects, 'novelty_list' : novelty_list, 'genres': genres})
 
 def get_anime(request):
     page_number = request.GET.get('page', 0)
     order_by_param = request.GET.get('order_by', 'title')
+    selected_genres = request.GET.getlist('selectedGenres[]', [])
+    selected_types = request.GET.getlist('selectedTypes[]', []) # получения значений фильтрации типов
+    print(selected_types)
     
+    if selected_genres:
+        anime = Anime.objects.filter(genres__id__in=selected_genres).annotate(genre_count=Count('genres')).filter(genre_count=len(selected_genres))
+        films = Film.objects.filter(genres__id__in=selected_genres).annotate(genre_count=Count('genres')).filter(genre_count=len(selected_genres))
+    else:
+        anime = Anime.objects.all()
+        films = Film.objects.all()
     
-    anime = Anime.objects.all()
-    films = Film.objects.all()
+    if selected_types:
+        anime = anime.filter(anime_type__in=selected_types)
+        films = films.filter(anime_type__in=selected_types)
+
     combined_objects = list(chain(anime, films))
-    sorted_objects = insertion_sort(combined_objects) # Сортировка никак не относится к сортировке на сайте
-    
-    
+    sorted_objects = insertion_sort(combined_objects)
+
     if order_by_param == 'created_at':
         # Если сортировка по дате, реверсируем список
         sorted_objects = sorted(sorted_objects, key=lambda obj: getattr(obj, order_by_param), reverse=True)
     else:
         sorted_objects = sorted(sorted_objects, key=lambda obj: getattr(obj, order_by_param))
+
     
     paginator = Paginator(sorted_objects, 12)  # Измените на нужный размер страницы
-
     try:
         page_objects = paginator.page(page_number)
-        
     except:
         return JsonResponse([], safe=False)
     
