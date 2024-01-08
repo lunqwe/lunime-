997d450a06ef165b4f1a9032c235cdc32fd6418f
lunqwe <roma.boyko10068@gmail.com>
2024-01-09 01:39:56 +0200

    12121

--- main/main/settings.py
+++ main/main/settings.py
@@ -20,9 +20,9 @@ from pathlib import Path
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
